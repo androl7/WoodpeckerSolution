0c398390667879926e5ee2696cbe996b9cd4568c
Adam <adamandrys7@gmail.com>
2018-08-14 21:31:05 +0200

    Add optimized method

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 public class Main {
 
@@ -10,7 +8,7 @@ public class Main {
         long[] listB = {3, 5, 6, 7, 10, 12, 15, 15, 16, 16, 20, 20, 20};
 
         //print results
-        ArrayList<ArrayList<Long>> resultsArray = convertLists2(listA, listB);
+        ArrayList<ArrayList<Long>> resultsArray = convertLists(listA, listB);
         for (ArrayList<Long> array : resultsArray) {
             System.out.println(array.toString());
         }
@@ -18,52 +16,6 @@ public class Main {
     }
 
     private static ArrayList<ArrayList<Long>> convertLists(long[] listA, long[] listB) {
-
-        //Convert Arrays to ArrayLists without duplicates
-        //A
-        Set<Long> listAWithoutDuplicates = new LinkedHashSet<>();
-
-        for (long aListA : listA) {
-            listAWithoutDuplicates.add(aListA);
-        }
-
-        ArrayList<Long> arrayListA = new ArrayList<>(listAWithoutDuplicates);
-
-        //B
-        Set<Long> listBWithoutDuplicates = new LinkedHashSet<>();
-
-        for (long aListB : listB) {
-            listBWithoutDuplicates.add(aListB);
-        }
-
-        ArrayList<Long> arrayListB = new ArrayList<>(listBWithoutDuplicates);
-
-
-        //1.
-        ArrayList<Long> resultList1 = new ArrayList<>(arrayListB);
-        resultList1.removeAll(arrayListA);
-
-        //2.
-        ArrayList<Long> resultList2 = new ArrayList<>(arrayListA);
-
-        resultList2.removeAll(arrayListB);
-
-        //3.
-        ArrayList<Long> resultList3 = new ArrayList<>(arrayListA);
-
-        resultList3.retainAll(arrayListB);
-
-
-        //add 3 arrayLists to returnArrayList
-        ArrayList<ArrayList<Long>> returnArrayList = new ArrayList<>();
-        returnArrayList.add(resultList1);
-        returnArrayList.add(resultList2);
-        returnArrayList.add(resultList3);
-
-        return returnArrayList;
-    }
-
-    private static ArrayList<ArrayList<Long>> convertLists2(long[] listA, long[] listB) {
 
         ArrayList<Long> resultList1 = new ArrayList<>();
         ArrayList<Long> resultList2 = new ArrayList<>();
